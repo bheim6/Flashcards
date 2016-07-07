0c87f65b6d04e17812cd6389af3fa2e6cfb4d671
Brian Heim <bheim67@gmail.com>
2016-07-07 15:12:18 -0600

    Actually completed I5

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -5,10 +5,10 @@ require_relative 'card'
 require_relative 'guess'
 require_relative 'round'
 
-card_1 = Card.new("What is 5 + 5", "10")
-card_2 = Card.new("What is Rachel's favorite animal?", "red pandas")
+card_1 = Card.new("What is 5 + 5?", "10")
+card_2 = Card.new("What is Rachel's favorite animal?", "red panda")
 card_3 = Card.new("What is Mikes's middle name?", "nobody knows")
-card_4 = Card.new("What cardboard cutout lives as Turing?", "Justin Bieber")
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin bieber")
 
 deck = Deck.new([card_1, card_2, card_3, card_4])
 
--- lib/round.rb
+++ lib/round.rb
@@ -34,30 +34,33 @@ class Round
   def start
     puts "Welcome! You're playing with #{deck.count} cards."
     puts "-------------------------------------------------"
+
     puts "This is card number #{@round_count + 1} out of #{deck.count}."
-
     puts "Question: #{current_card.question}"
     guess = gets.chomp
     record_guess(guess, current_card)
     puts @guesses.first.feedback
 
+    puts "This is card number #{@round_count + 1} out of #{deck.count}."
     puts "Question: #{current_card.question}"
     guess = gets.chomp
     record_guess(guess, current_card)
-    puts @guesses.first.feedback
+    puts @guesses[1].feedback
 
+    puts "This is card number #{@round_count + 1} out of #{deck.count}."
     puts "Question: #{current_card.question}"
     guess = gets.chomp
     record_guess(guess, current_card)
-    puts @guesses.first.feedback
+    puts @guesses[2].feedback
 
+    puts "This is card number #{@round_count + 1} out of #{deck.count}."
     puts "Question: #{current_card.question}"
     guess = gets.chomp
     record_guess(guess, current_card)
-    puts @guesses.first.feedback
+    puts @guesses[3].feedback
 
     puts "****** Game over! ******"
-    puts "You had #{number_correct} correct guesses out of #{deck.count} for a score of #{percent_correct}%."
+    puts "You had #{number_correct} correct guesses out of #{deck.count} for a score of #{percent_correct.to_i}%."
   end
 
 
